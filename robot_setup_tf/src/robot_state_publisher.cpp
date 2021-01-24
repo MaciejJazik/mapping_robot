d0cd6630e47543972d74f649756321af1a51f8e5
Maciej <maciej.jazik@gmail.com>
2021-01-24 19:37:52 +0100

    tf setup for kinect+hokuyo

--- robot_setup_tf/src/robot_state_publisher.cpp
+++ robot_setup_tf/src/robot_state_publisher.cpp
@@ -32,17 +32,27 @@ int main(int argc, char** argv)
     broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(tf::createQuaternionFromRPY(0.0, 0.0, 0.0)), tf::Vector3(0.0, -0.1827, 1.216)),
+        ros::Time::now(),"base_footprint", "hokuyo"));
+        
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(tf::createQuaternionFromRPY(0.0, 0.0, 0.0)), tf::Vector3(0.0, 0.0, 0.0)),
+        ros::Time::now(),"hokuyo", "hokuyo_link"));
+
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(tf::createQuaternionFromRPY(0.0, 0.0, 0.0)), tf::Vector3(0.0, 0.0, 0.0)),
+        ros::Time::now(),"hokuyo_link", "camera_depth_frame"));
+		
+		broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(tf::createQuaternionFromRPY(0.0, 0.0, 0.0)), tf::Vector3(0.0, -0.1827, 1.216)),
         ros::Time::now(),"base_footprint", "kinect"));
         
     broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(tf::createQuaternionFromRPY(0.0, 0.0, 0.0)), tf::Vector3(0.0, 0.0, 0.0)),
         ros::Time::now(),"kinect", "camera_link"));
-
-    broadcaster.sendTransform(
-      tf::StampedTransform(
-        tf::Transform(tf::Quaternion(tf::createQuaternionFromRPY(0.0, 0.0, 0.0)), tf::Vector3(0.0, 0.0, 0.0)),
-        ros::Time::now(),"camera_link", "camera_depth_frame"));
 
         
 	/*broadcaster.sendTransform(
